37dc709fa7d0da9b3c70e43b17cec813776cb8c9
emilyfu1 <1emilyfu@gmail.com>
2025-12-02 12:22:57 +0000

    add characteristics

--- datacleaning.R
+++ datacleaning.R
@@ -19,18 +19,20 @@ who_invalid = c(996, 997, 998)
 variables = c("YEAR", "STATEFIP", "COUNTY", "SERIAL", "person_id", "AGE", 
               "SEX", "RACE", "EDUCYRS", "UHRSWORKT", "EARNWEEK", 
               "hh_total_earn", "earn_share", "HH_NUMKIDS", "total_leisure", 
-              "total_private_leisure", "total_childcare", 
+              "total_private_leisure", "total_leisure_r", 
+              "total_private_leisure_r", "total_childcare", 
               "total_childcare_nospouse", "KID1TO2", "KID3TO5", "KID6TO12", 
               "KID13TO17", "spouse_earnweek", "spouse_usualhours", 
-              "spouse_educ")
+              "spouse_educ", "spouse_race")
 
 # childcare activities (currently not used)
 childcare_actlines = c(030101, 030102, 030103, 030104, 030105, 030106, 030107, 
                        030108, 030109, 030110, 030111, 030112, 030199, 030201, 
                        030202, 030203, 030204, 030299, 030301, 030302, 030303, 
-                       030399, 080101, 080102, 080199, 160107, 180301, 180302, 
-                       180303, 180304, 180801)
+                       030399, 080101, 080102, 080199, 160103, 160107, 180301, 
+                       180302, 180303, 180304, 180801)
 
+# with eating, sleeping and washing
 leisure_actlines = c(010101, 010102, 010199, 010301, 010399, 010401, 010499, 
                      019999, 050201, 020603, 120201, 120202, 120299, 120301,
                      120302, 120303, 120304, 120305, 120306, 120307, 120308,
@@ -53,29 +55,34 @@ leisure_actlines = c(010101, 010102, 010199, 010301, 010399, 010401, 010499,
                      150402, 150499, 150500, 150501, 150599, 150601, 150602,
                      150699, 150701, 150799, 150801, 150899, 159999, 180805,
                      181201, 181202, 181203, 181204, 181205, 181206, 181299,
-                     181300, 181301, 181302, 181401, 181499, 181501, 181599)
+                     181300, 181301, 181302, 181401, 181499, 181501, 181599,
+                     010201, 010299, 060102, 060201, 060202, 060203, 060204,
+                     060299, 060302, 060402, 070104, 110101, 110201, 110299, 
+                     119999, 120101, 120199, 160101, 160102, 050202, 080501, 
+                     080502, 080599, 181101, 181199)
 
-# not including religious and volunteering activities
-leisure_restricted = c(010101, 010102, 010199, 010301, 010399, 010401, 010499, 
-                     019999, 050201, 020603, 120201, 120202, 120299, 120301,
-                     120302, 120303, 120304, 120305, 120306, 120307, 120308,
-                     120309, 120310, 120311, 120312, 120313, 120399, 120401, 
-                     120402, 120403, 120404, 120405, 120499, 120501, 120502,
-                     120503, 120504, 120599, 129999, 130101, 130102, 130103,
-                     130104, 130105, 130106, 130107, 130108, 130109, 130110,
-                     130111, 130112, 130113, 130114, 130115, 130116, 130117,
-                     130118, 130119, 130120, 130121, 130122, 130123, 130124,
-                     130125, 130126, 130127, 130128, 130129, 130130, 130131,
-                     130132, 130133, 130134, 130135, 130136, 130199, 130201,
-                     130202, 130203, 130204, 130205, 130206, 130207, 130208, 
-                     130209, 130210, 130211, 130212, 130213, 130214, 130215,
-                     130216, 130217, 130218, 130219, 130220, 130221, 130222,
-                     130223, 130224, 130225, 130226, 130227, 130228, 130229,
-                     130230, 130231, 130232, 130299, 130301, 130302, 130399,
-                     140101, 140102, 140103, 140104, 140105, 149999,180805,
-                     181201, 181202, 181203, 181204, 181205, 181206, 181299,
-                     181300, 181301, 181302)
-
+# relaxing, socialising, sport, entertainment, and hobbies only
+restrict_actlines = c(050201, 020603, 
+                     120201, 120202, 120299, 120301, 120302, 120303, 120304, 
+                     120305, 120306, 120307, 120308, 120309, 120310, 120311, 
+                     120312, 120313, 120399, 120401, 120402, 120403, 120404, 
+                     120405, 120499, 120501, 120502, 120503, 120504, 120599, 
+                     129999, 130101, 130102, 130103, 130104, 130105, 130106, 
+                     130107, 130108, 130109, 130110, 130111, 130112, 130113, 
+                     130114, 130115, 130116, 130117, 130118, 130119, 130120, 
+                     130121, 130122, 130123, 130124, 130125, 130126, 130127, 
+                     130128, 130129, 130130, 130131, 130132, 130133, 130134, 
+                     130135, 130136, 130199, 130201, 130202, 130203, 130204, 
+                     130205, 130206, 130207, 130208, 130209, 130210, 130211, 
+                     130212, 130213, 130214, 130215, 130216, 130217, 130218, 
+                     130219, 130220, 130221, 130222, 130223, 130224, 130225, 
+                     130226, 130227, 130228, 130229, 130230, 130231, 130232, 
+                     130299, 130301, 130302, 130399, 140101, 140102, 140103, 
+                     140104, 140105, 149999, 180805, 181201, 181202, 181203, 
+                     181204, 181205, 181206, 181299, 181300, 181301, 181302, 
+                     181401, 181499, 010299, 060102, 060201, 
+                     060202, 060203, 060204, 060299, 060302, 060402, 070104, 
+                     110101, 120101, 120199, 160101, 160102)
 
 # package download message
 if (!require("ipumsr")) stop("Reading IPUMS data into R requires the ipumsr package. It can be installed using the following command: install.packages('ipumsr')")
@@ -211,6 +218,11 @@ data_working_parents = data_adults |>
     spouse_educ = if (any(SPEDUC < invalid_educ, na.rm = TRUE)) {
       max(SPEDUC[SPEDUC < invalid_educ], na.rm = TRUE)
     } else NA_real_,
+    
+    # respondent-reported spouse race, fill to household level
+    spouse_race = if (any(SPRACE < invalid_race, na.rm = TRUE)) {
+      max(SPRACE[SPRACE < invalid_race], na.rm = TRUE)
+    } else NA_real_,
 
     # fill in respondent-reported kid dummy variables to household level
     KID1TO2 = if (any(KID1TO2 < invalid_kid_dummy, na.rm = TRUE)) {
@@ -256,29 +268,41 @@ activity_summaries = data_working_parents |>
   inner_join(data_activities, by = c("YEAR", "SERIAL", "LINENO")) |>
   # identify categories
   mutate(activity_is_leisure = ACTIVITY %in% leisure_actlines,
+         activity_is_leisure_r = ACTIVITY %in% restrict_actlines,
+         activity_is_childcare = ACTIVITY %in% childcare_actlines,
          activity_private = !(RELATEWU %in% who_private_exclude) &
            !(RELATEWU %in% who_invalid),
          activity_excludesspouse = !(RELATEWU %in% who_partner),
-         private_leisure = activity_is_leisure & activity_private) |>
+         private_leisure = activity_is_leisure & activity_private,
+         private_leisure_r = activity_is_leisure_r & activity_private) |>
   
   # collapse to person-level time allocations
   group_by(YEAR, SERIAL, person_id) |>
   summarise(total_leisure = sum(DURATION_EXT[activity_is_leisure], na.rm=TRUE),
             total_private_leisure = sum(DURATION_EXT[private_leisure], na.rm=TRUE),
-            total_childcare = sum(SCC_OWNHH_LN, na.rm=TRUE),
-            total_childcare_nospouse = sum(SCC_OWNHH_LN[activity_excludesspouse], na.rm=TRUE),
+            total_leisure_r = sum(DURATION_EXT[activity_is_leisure_r], na.rm=TRUE),
+            total_private_leisure_r = sum(DURATION_EXT[private_leisure_r], na.rm=TRUE),
+            # to create the childcare variable, combine activities and secondary
+            total_childcare_act = sum(DURATION_EXT[activity_is_childcare], na.rm=TRUE),
+            total_childcare_sec = sum(SCC_OWN_LN, na.rm=TRUE),
+            total_childcare = max(total_childcare_act, total_childcare_sec),
+            total_childcare_nospouse = sum(total_childcare[activity_excludesspouse], na.rm=TRUE),
             .groups="drop")
 
 # keep only households where parents have non-zero private leisure
 valid_households = activity_summaries |>
   group_by(YEAR, SERIAL) |>
   summarise(filtered_household_size = n(),
+            # check at household level if activity requirements are met
             both_private_leisure = all(total_private_leisure > 0),
-            # maybe need that one parent does some childcare
+            both_private_leisure_r = all(total_private_leisure_r > 0),
             any_childcare = any(total_childcare > 0),
             .groups="drop") |>
+  # need some private leisure activity taken by both people
   filter(filtered_household_size == 2,
-         both_private_leisure)
+         # maybe need that at least one parent does some childcare
+         # any_childcare == 1,
+         both_private_leisure_r)
 
 # get back to individual-level data
 final_individual_data = data_working_parents |>
@@ -294,10 +318,13 @@ final_individual_data = final_individual_data |>
          # time use in hours
          total_leisure_h = total_leisure / 60,
          total_private_leisure_h = total_private_leisure / 60,
+         total_leisure_h_r = total_leisure_r / 60,
+         total_private_leisure_h_r = total_private_leisure_r / 60,
          total_childcare_h = total_childcare / 60,
          total_childcare_nospouse_h = total_childcare_nospouse / 60,
          # private leisure expenditure
          leisure_exp = hrly_wage * total_private_leisure_h,
+         leisure_exp_r = hrly_wage * total_private_leisure_h_r,
          # education in levels
          educ_cat = case_when(EDUCYRS < 200 ~ 0, # below high school
                               # high school to some college
@@ -312,11 +339,22 @@ final_individual_data = final_individual_data |>
                                      # completed undergraduate/graduate degrees
                                      spouse_educ >= 217 & spouse_educ <= 321 ~ 2,
                                      TRUE ~ NA_real_),
+         
          # sex specific wages
          wage_f = if_else(SEX == 2, hrly_wage, spouse_hrly_wage),
          wage_m = if_else(SEX == 1, hrly_wage, spouse_hrly_wage),
+         
+         # sex specific education
          educ_f = if_else(SEX == 2, educ_cat, spouse_educ_cat),
-         educ_m = if_else(SEX == 1, educ_cat, spouse_educ_cat)) |>
+         educ_m = if_else(SEX == 1, educ_cat, spouse_educ_cat),
+         
+         # sex specific race
+         race_f = if_else(SEX == 2, RACE, spouse_race),
+         race_m = if_else(SEX == 1, RACE, spouse_race),
+         
+         # sex specific usual hours worked
+         uhrsworkt_f = if_else(SEX == 2, UHRSWORKT, spouse_usualhours),
+         uhrsworkt_m = if_else(SEX == 1, UHRSWORKT, spouse_usualhours)) |>
   
   # household-level constructs
   group_by(YEAR, SERIAL) |>
@@ -325,9 +363,16 @@ final_individual_data = final_individual_data |>
          # the household budget
          y = (wage_f + wage_m) * 24,
          
-         # leisure expenditure
+         # gender-specific leisure expenditure
          leisure_exp_f = sum(leisure_exp[SEX==2]),
-         leisure_exp_m = sum(leisure_exp[SEX==1])) |> 
+         leisure_exp_m = sum(leisure_exp[SEX==1]),
+         leisure_exp_f_r = sum(leisure_exp_r[SEX==2]),
+         leisure_exp_m_r = sum(leisure_exp_r[SEX==1]),
+         
+         # gender-specific childcare)
+         total_childcare_nospouse_h_f = sum(total_childcare_nospouse_h[SEX==2]),
+         total_childcare_nospouse_h_m = sum(total_childcare_nospouse_h[SEX==1])
+         ) |> 
   ungroup() |>
   
   # since it's a pooled sample? 
--- sharing_variables.R
+++ sharing_variables.R
@@ -9,37 +9,47 @@ hh = final_individual_data |>
   group_by(YEAR, SERIAL) |>   # unique household identifier
   summarise(
     # household leisure expenditure by gender
-    leisure_exp_f = mean(leisure_exp_f, na.rm=TRUE),
-    leisure_exp_m = mean(leisure_exp_m, na.rm=TRUE),
+    leisure_exp_f = max(leisure_exp_f, na.rm=TRUE),
+    leisure_exp_m = max(leisure_exp_m, na.rm=TRUE),
+    leisure_exp_f_r = max(leisure_exp_f_r, na.rm=TRUE),
+    leisure_exp_m_r = max(leisure_exp_m_r, na.rm=TRUE),
+    total_childcare_nospouse_h_f = max(total_childcare_nospouse_h_f),
+    total_childcare_nospouse_h_m = max(total_childcare_nospouse_h_m),
     
     # total household budget
-    y = mean(y, na.rm=TRUE),
+    y = max(y, na.rm=TRUE),
     
     # sex-specific variables
-    wage_f = mean(wage_f, na.rm=TRUE),
-    wage_m = mean(wage_m, na.rm=TRUE),
-    educ_f = mean(educ_f, na.rm=TRUE),
-    educ_m = mean(educ_m, na.rm=TRUE),
+    wage_f = max(wage_f, na.rm=TRUE),
+    wage_m = max(wage_m, na.rm=TRUE),
+    educ_f = max(educ_f, na.rm=TRUE),
+    educ_m = max(educ_m, na.rm=TRUE),
+    race_f = max(race_f, na.rm=TRUE),
+    race_m = max(race_m, na.rm=TRUE),
+    race_f = max(race_f, na.rm=TRUE),
+    race_m = max(race_m, na.rm=TRUE),
+    uhrsworkt_f = max(uhrsworkt_f, na.rm=TRUE),
+    uhrsworkt_m = max(uhrsworkt_m, na.rm=TRUE),
     
     # age variables (your grouped variables already computed these correctly)
-    avg_age = mean(avg_age, na.rm=TRUE),
-    age_gap = mean(age_gap, na.rm=TRUE),
+    avg_age = max(avg_age, na.rm=TRUE),
+    age_gap = max(age_gap, na.rm=TRUE),
     
     # include deviations
-    dev_wage_f = mean(dev_wage_f, na.rm=TRUE),
-    dev_wage_m = mean(dev_wage_m, na.rm=TRUE),
-    dev_educ_f = mean(dev_educ_f, na.rm=TRUE),
-    dev_educ_m = mean(dev_educ_m, na.rm=TRUE),
-    dev_avg_age = mean(dev_avgage, na.rm=TRUE),
-    dev_age_gap = mean(dev_agegap, na.rm=TRUE),
+    dev_wage_f = max(dev_wage_f, na.rm=TRUE),
+    dev_wage_m = max(dev_wage_m, na.rm=TRUE),
+    dev_educ_f = max(dev_educ_f, na.rm=TRUE),
+    dev_educ_m = max(dev_educ_m, na.rm=TRUE),
+    dev_avg_age = max(dev_avgage, na.rm=TRUE),
+    dev_age_gap = max(dev_agegap, na.rm=TRUE),
     
     # interaction terms for the SUR
-    Bx_dev_wage_f = mean(Bx_dev_wage_f, na.rm=TRUE),
-    Bx_dev_wage_m = mean(Bx_dev_wage_m, na.rm=TRUE),
-    Bx_dev_educ_f = mean(Bx_dev_educ_f, na.rm=TRUE),
-    Bx_dev_educ_m = mean(Bx_dev_educ_m, na.rm=TRUE),
-    Bx_dev_avgage = mean(Bx_dev_avgage, na.rm=TRUE),
-    Bx_dev_agegap = mean(Bx_dev_agegap, na.rm=TRUE),
+    Bx_dev_wage_f = max(Bx_dev_wage_f, na.rm=TRUE),
+    Bx_dev_wage_m = max(Bx_dev_wage_m, na.rm=TRUE),
+    Bx_dev_educ_f = max(Bx_dev_educ_f, na.rm=TRUE),
+    Bx_dev_educ_m = max(Bx_dev_educ_m, na.rm=TRUE),
+    Bx_dev_avgage = max(Bx_dev_avgage, na.rm=TRUE),
+    Bx_dev_agegap = max(Bx_dev_agegap, na.rm=TRUE),
     
     .groups = "drop")
 
